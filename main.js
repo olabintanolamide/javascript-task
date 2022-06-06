a8b05c781e55efdfc97a2fdd2b45783915934570
olamide <olabintanolamide145@gmail.com>
2022-06-06 14:36:45 +0100

    seting up

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const registerBtn = document.getElementsByClassName("register")[0];
 const loginBtn = document.getElementsByClassName("login")[0];
 const closeBtn = document.getElementsByClassName("close")[0];
 const secondCloseBtn = document.getElementsByClassName("close2")[0];
-
+const thirdCloseBtn = document.getElementsByClassName("close3")[0];
 
 registerBtn.addEventListener("click", () => {
   modal.style.display = "block";
@@ -16,35 +16,81 @@ registerBtn.addEventListener("click", () => {
 
 loginBtn.addEventListener("click", () => {
   secondModal.style.display = "block";
-})
-
-
-  closeBtn.addEventListener("click", () => {
-      modal.style.display = "none";
-  });
-
-  secondCloseBtn.addEventListener("click", () => {
-    secondModal.style.display = "none";
-  });
-
-
-const submitBtn = document.getElementsByClassName("submit")[0];
-const testing = document.getElementById("testing");
-const getStarted = document.getElementsByClassName("submit")
-
-submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log('hello');
-    testing.style.display = "block";
-    modal.style.display = "none";
 });
 
-const adding = document.getElementById("adding")[0];
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+secondCloseBtn.addEventListener("click", () => {
+  secondModal.style.display = "none";
+});
+
+thirdCloseBtn.addEventListener("click", () => {
+  shoppingModal.style.display = "none";
+});
+
+const submitBtn = document.getElementsByClassName("submit")[0];
+const getStarted = document.getElementsByClassName("submit");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("hello");
+  modal.style.display = "none";
+});
+
+//const ulTag = document.getElementById("class-list");
+const genLi = document.createElement("li");
 const addItem = document.getElementsByClassName("addItem")[0];
 const shoppingModal = document.getElementById("shopping-myModal");
+const itemBtn = document.getElementsByClassName("item-button")[0];
+const getItem = document.getElementsByClassName("get-item")[0];
 
-adding.addEventListener("click", () => {
-    shoppingModal.style.display = "block";
-    console.log("help!!!")
-})
+addItem.addEventListener("click", (e) => {
+  e.preventDefault();
+  // genLi.textContent = "fight";
+  // ulTag.append(genLi);
+  shoppingModal.style.display = "none";
+});
 
+getItem.addEventListener("click", (e) => {
+  e.preventDefault();
+  itemBtn.style.display = "block";
+  secondModal.style.display = "none";
+});
+
+itemBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  console.log("hey!");
+  shoppingModal.style.display = "block";
+});
+
+const inputText = document.querySelector("#shopping-item");
+const myButton = document.querySelector('.addItem');
+const list = document.querySelector(".container ul");
+
+myButton.addEventListener('click', (e) => {
+  if(inputText.value != ""){
+    e.preventDefault();
+  //creating li
+    const myLi = document.createElement('li');
+    myLi.innerHTML = inputText.value;
+    list.appendChild(myLi);
+  //creating span
+  const mySpan = document.createElement('span');
+  mySpan.innerHTML = 'x';
+  myLi.appendChild(mySpan);
+  }
+  inputText.value = "";
+});
+
+// const closeList = document.querySelectorAll('span');
+
+
+// for(let i = 0; i<closeList.length; i++){
+//   close[i].addEventListener('click', () => {
+//     close[i].parentElement.style.display = "none";
+//     console.log("work")
+//   })
+// }
